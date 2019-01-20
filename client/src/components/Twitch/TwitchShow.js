a7b6986ab12253d932990ca75104a57c3bbb0512
besSejrani <besjan.sejrani@outlook.fr>
2019-01-20 20:57:51 +0100

    final commit with OBS streaming

--- client/src/components/Twitch/TwitchShow.js
+++ client/src/components/Twitch/TwitchShow.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class TwitchShow extends Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.props.twitch) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  };
 
   render() {
     const { twitch } = this.props;
@@ -15,6 +45,7 @@ class TwitchShow extends Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{twitch.title}</h1>
         <h4>{twitch.description}</h4>
       </div>
